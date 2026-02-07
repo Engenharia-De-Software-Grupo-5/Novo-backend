f8cd40653af9526e8e6d8c0c49fc983edb3487da
ViniciusLaureano <viniciusglaureano@gmail.com>
2026-02-07 17:52:51 -0300

    docs(examples): create and docs responses and dtos for base examples

---
+++ src/contracts/examples/example2.response.ts
@@ -0,0 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class Example2Response {
+  @ApiProperty({
+    description: 'descrição de exemplo para campo obrigatório',
+    example: 'conteúdo de exemplo',
+  })
+  campo1: string;
+
+  @ApiPropertyOptional({
+    description: 'descrição de exemplo para campo não obrigatório',
+    example: 'conteúdo de exemplo',
+  })
+  campo2?: string;
+}
