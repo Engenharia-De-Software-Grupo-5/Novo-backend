f903a682bd2c2b3a08b089f3f5f92df0c7b6cb9e
Pedro-Henriques-BP <140713917+Pedro-Henriques-BP@users.noreply.github.com>
2026-02-14 16:59:46 -0300

    atualizando_migrations

--- prisma/schema/migrations/20260214195308_condominiums_module/migration.sql
+++ prisma/schema/migrations/20260214195308_condominiums_module/migration.sql
@@ -4,6 +4,12 @@ CREATE TYPE "UnityType" AS ENUM ('HOUSE', 'APARTMENT', 'CONMERCIAL_ROOM');
 -- CreateEnum
 CREATE TYPE "PropertySituation" AS ENUM ('ACTIVE', 'INACTIVE', 'UNAVAILABLE', 'MAINTENANCE');
 
+-- CreateEnum
+CREATE TYPE "ContractType" AS ENUM ('CLT', 'PJ', 'TEMPORARY', 'INTERNSHIP', 'OUTSOURCED');
+
+-- CreateEnum
+CREATE TYPE "EmployeeStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'TERMINATED', 'ON_LEAVE', 'VACATION');
+
 -- CreateTable
 CREATE TABLE "condominiums" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
@@ -54,14 +60,50 @@ CREATE TABLE "properties" (
     CONSTRAINT "properties_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "employees" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "cpf" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "bankDataId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "role" TEXT NOT NULL,
+    "contractType" "ContractType" NOT NULL,
+    "hireDate" TIMESTAMP(3) NOT NULL,
+    "terminationDate" TIMESTAMP(3),
+    "baseSalary" DOUBLE PRECISION NOT NULL,
+    "workload" INTEGER NOT NULL,
+    "status" "EmployeeStatus" NOT NULL DEFAULT 'ACTIVE',
+
+    CONSTRAINT "employees_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "banksdata" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "banksdata_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "condominiums_name_key" ON "condominiums"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "properties_identifier_key" ON "properties"("identifier");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "employees_cpf_key" ON "employees"("cpf");
+
 -- AddForeignKey
 ALTER TABLE "condominiums" ADD CONSTRAINT "condominiums_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "addresses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "properties" ADD CONSTRAINT "properties_condominiumId_fkey" FOREIGN KEY ("condominiumId") REFERENCES "condominiums"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "employees" ADD CONSTRAINT "employees_bankDataId_fkey" FOREIGN KEY ("bankDataId") REFERENCES "banksdata"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
