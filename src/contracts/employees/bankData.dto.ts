746664fa8eeeea62870a1a5bc542fb93997eb9a5
Pedro-Henriques-BP <140713917+Pedro-Henriques-BP@users.noreply.github.com>
2026-02-25 13:35:47 -0300

    Uptade BankData of employees

--- src/contracts/employees/bankData.dto.ts
+++ src/contracts/employees/bankData.dto.ts
@@ -1,3 +1,36 @@
-export class BankDataDto{
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
+export class BankDataDto{
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+    description: 'Bank name',
+    example: 'Banco do Brasil',
+    })
+    bank: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+    description: 'Type of bank account',
+    example: 'Checking',
+    })
+    accountType: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+    description: 'Bank account number',
+    example: '123456-7',
+    })
+    accountNumber: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+    description: 'Bank agency number',
+    example: '0001',
+    })
+    agency: string;
 }
