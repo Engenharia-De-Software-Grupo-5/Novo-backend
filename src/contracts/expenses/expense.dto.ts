0668b784c7aa104bc3697cbcdfa19052f73e80c6
theo-rodrigues <theo.evangelista.rodrigues@ccc.ufcg.edu.br>
2026-02-26 20:49:43 -0300

    refactor(expenses): refactor expenses routes and add description property

--- src/contracts/expenses/expense.dto.ts
+++ src/contracts/expenses/expense.dto.ts
@@ -7,6 +7,10 @@ export class ExpenseDto {
   @IsEnum(ExpenseTargetType)
   targetType: ExpenseTargetType;
 
+  @ApiProperty({ description: 'Expense major description', example: 'WATER' })
+  @IsString()
+  description: string;
+
   @ApiProperty({ description: 'Property id when targetType=PROPERTY', example: '123e4567-e89b-12d3-a456-426614174001', required: false })
   @IsString()
   propertyId?: string;
