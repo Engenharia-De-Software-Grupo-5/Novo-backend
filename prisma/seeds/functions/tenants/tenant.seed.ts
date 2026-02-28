f25afab2ecbb4dc37422e2e3bc475ea4d790c7fa
ViniciusLaureano <viniciusglaureano@gmail.com>
2026-02-28 20:35:04 -0300

    fix seed rg

--- prisma/seeds/functions/tenants/tenant.seed.ts
+++ prisma/seeds/functions/tenants/tenant.seed.ts
@@ -1,16 +1,11 @@
 import { PrismaClient, TenantStatus } from '@prisma/client';
 
-export async function seedTenants(
-  prisma: PrismaClient,
-) {
-
+export async function seedTenants(prisma: PrismaClient) {
   const address = await prisma.addresses.findFirst();
   const condominium = await prisma.condominiums.findFirst();
 
   if (!address || !condominium) {
-    throw new Error(
-      'You must seed addresses and condominiums before tenants.',
-    );
+    throw new Error('You must seed addresses and condominiums before tenants.');
   }
 
   const tenantA = await prisma.tenants.create({
@@ -20,13 +15,13 @@ export async function seedTenants(
       rg: '123456789',
       issuingAuthority: 'SSP/PE',
 
-      birthDate: "1995-03-15",
+      birthDate: '1995-03-15',
       maritalStatus: 'SINGLE',
       monthlyIncome: 5000,
       email: 'tenantA@email.com',
       primaryPhone: '+55 83 99999-0001',
 
-      address: "123 Main St, Apt 4B, Cityville",
+      address: '123 Main St, Apt 4B, Cityville',
       condominiumId: condominium.id,
 
       status: TenantStatus.ACTIVE,
@@ -37,16 +32,16 @@ export async function seedTenants(
     data: {
       name: 'Tenant B',
       cpf: '12345678901',
-      rg: '123456789',
+      rg: '123456788',
       issuingAuthority: 'SSP/PE',
 
-      birthDate: "1990-01-01",
+      birthDate: '1990-01-01',
       maritalStatus: 'MARRIED',
       monthlyIncome: 7500,
       email: 'tenantB@email.com',
       primaryPhone: '+55 83 99999-0002',
 
-      address: "123 Main St, Apt 4B, Cityville",
+      address: '123 Main St, Apt 4B, Cityville',
       condominiumId: condominium.id,
 
       status: TenantStatus.ACTIVE,
