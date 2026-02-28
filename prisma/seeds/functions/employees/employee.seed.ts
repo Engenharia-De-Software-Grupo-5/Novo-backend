673233acea5fae1cd495f494076bc1f1df176886
ViniciusLaureano <viniciusglaureano@gmail.com>
2026-02-28 20:32:38 -0300

    fix seed

--- prisma/seeds/functions/employees/employee.seed.ts
+++ prisma/seeds/functions/employees/employee.seed.ts
@@ -1,47 +1,49 @@
 import { PrismaClient } from '@prisma/client';
 
 export async function seedEmployees(
+  condominiumId: string,
   prisma: PrismaClient,
 ) {
-
-    const employee1 = await prisma.employees.create({
-        data: { 
-            cpf: '12312312312',
-            name: 'Oswaldo Fernandes',
-            condominium: { connect: { id: 'cond-123' } },
-            bankData: { create: {
-                bank: 'Banco do Brasil',
-                accountNumber: '123412789',
-                agency: '0002',
-                accountType: 'Corrente',
-            },
-            },
-            status: 'ACTIVE',
-            role: 'PORTEIRO',
-            admissionDate: '2013-01-27',
-            birthDate: '1990-01-01',
+  const employee1 = await prisma.employees.create({
+    data: {
+      cpf: '12312312312',
+      name: 'Oswaldo Fernandes',
+      condominium: { connect: { id: condominiumId } },
+      bankData: {
+        create: {
+          bank: 'Banco do Brasil',
+          accountNumber: '123412789',
+          agency: '0002',
+          accountType: 'Corrente',
         },
-    });
+      },
+      status: 'ACTIVE',
+      role: 'PORTEIRO',
+      admissionDate: '2013-01-27',
+      birthDate: '1990-01-01',
+    },
+  });
 
-    const employee2 = await prisma.employees.create({
-        data: { 
-            cpf: '98765432100',
-            name: 'Maria Silva',
-            condominium: { connect: { id: 'cond-123' } },
-            bankData: { create: {
-                bank: 'Banco do Brasil',
-                accountNumber: '123456789',
-                agency: '0001',
-                accountType: 'Corrente',
-            },
-            },
-            status: 'ACTIVE',
-            role: 'PORTEIRO',
-            contractType: 'CLT',
-            admissionDate: '2025-02-03',
-            birthDate: '1990-01-01',
+  const employee2 = await prisma.employees.create({
+    data: {
+      cpf: '98765432100',
+      name: 'Maria Silva',
+      condominium: { connect: { id: condominiumId } },
+      bankData: {
+        create: {
+          bank: 'Banco do Brasil',
+          accountNumber: '123456789',
+          agency: '0001',
+          accountType: 'Corrente',
         },
-    });
-            
+      },
+      status: 'ACTIVE',
+      role: 'PORTEIRO',
+      contractType: 'CLT',
+      admissionDate: '2025-02-03',
+      birthDate: '1990-01-01',
+    },
+  });
+
   return { employee1, employee2 };
 }
